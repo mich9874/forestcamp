9d49803ae2ea76b197c76b0464c1aa3ceca3f7ab
mich9874 <73462339+mich9874@users.noreply.github.com>
2022-07-03 01:57:22 +0200

    Add files via upload
    update v1

---
+++ scirpt.js
@@ -0,0 +1,5 @@
+const menuicon = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menu");
+menuicon.addEventListener("click", () => {
+navbar.classList.toggle("change");
+});
