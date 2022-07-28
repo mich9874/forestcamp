04e179b2eb49bf2e46fdadf71ac68133b839ccad
mich9874 <73462339+mich9874@users.noreply.github.com>
2022-07-28 22:39:08 +0200

    update v3

---
+++ scirpt1.js
@@ -0,0 +1,6 @@
+const menuicon = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menu");
+menuicon.addEventListener("click", () => {
+navbar.classList.toggle("change");
+});
+
